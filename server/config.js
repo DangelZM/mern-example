b57130d4f66a3808ef6152dadef6d8ac10c93cc7
Ivan Zmerzlyi <dangelzm@gmail.com>
2017-06-14 14:03:50 +0300

    First commit

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
-  port: process.env.PORT || 8000,
+  mongoURL: process.env.MONGO_URL || 'mongodb://admin:532453@ds157469.mlab.com:57469/production_justcoders',
+  port: process.env.PORT || 3000,
 };
 
 export default config;
